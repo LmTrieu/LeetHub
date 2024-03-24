ffbd2b3e4edae8ccba61fed0b3cbced1adcfc984
LMTrieu <126946892+LmTrieu@users.noreply.github.com>
2024-03-24 23:20:53 +0700

    Time: 148 ms (75.79%), Space: 56 MB (32.43%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+       return intervals.OrderBy(interval => interval[0]) 
+            .Aggregate(new List<int[]>(), (acc, current) =>
+            {
+                if (acc.Count == 0 || acc.Last()[1] < current[0])
+                {
+                    acc.Add(current);
+                }
+                else
+                {
+                    acc.Last()[1] = Math.Max(acc.Last()[1], current[1]);
+                }
+                return acc;
+            })
+            .ToArray();
+    }
+}
